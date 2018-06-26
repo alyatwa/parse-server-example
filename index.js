f3bba4eb83d79597287dbe8a95dca3396b984f81
ali atwa <ali.atwa2040@gmail.com>
2018-06-27 01:59:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
+var SimpleSendGridAdapter = require('parse-server-sendgrid-adapter');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
@@ -27,45 +28,10 @@ var api = new ParseServer({
   // Your apps name. This will appear in the subject and body of the emails that are sent.
   appName: 'Parse App',
   // The email adapter
-  emailAdapter: {
-    module: require('parse-server-mailjet-adapter'),
-    options: {
-      // The API key from your Mailjet account
-      apiKey: process.env.YOUR_MAILJET_API_KEY,
-      // The API secret from your Mailjet account
-      apiSecret: process.env.YOUR_MAILJET_API_SECRET,
-      // The email to send Mailjet templates bug reports to
-      apiErrorEmail: "bugreport@yourdomain.com",
-      // The email address that your emails come from
-      fromEmail: "noreply@yourdomain.com",
-      // The name do display as the sender (optional)
-      fromName: "The sender name",
-      //
-      // Parameters for the reset password emails
-      //
-      // The subject of the email to reset the password
-      passwordResetSubject: "Reset My Password",
-      // Set it to use a template with your Mailjet account.
-      // This is the id of the template to use.
-      passwordResetTemplateId: 12345,
-      // If you do not use template, you can set the plain text part here
-      passwordResetTextPart: "Hi,\n\nYou requested to reset your password for {{var:appName}}.\n\nPlease, click here to set a new password: {{var:link}}",
-      // If you do not use template, you can set the html part here
-      passwordResetHtmlPart: "Hi,<p>You requested to reset your password for <b>{{var:appName}}</b>.</p><p>Please, click here to set a new password: {{var:link}}</p>",
-      //
-      // Parameters for the email verification emails
-      //
-      // The subject of the email to reset the password
-      verificationEmailSubject: "Verify your email",
-      // Set it to use a template with your Mailjet account.
-      // This is the id of the template to use.
-      verificationEmailTemplateId: 67890,
-      // If you do not use template, you can set the plain text part here
-      verificationEmailTextPart: "Hi,\n\nYou are being asked to confirm the e-mail address {{var:email}} with {{var:appName}}\n\nClick here to confirm it: {{var:link}}",
-      // If you do not use template, you can set the html part here
-      verificationEmailHtmlPart: "Hi,<p>You are being asked to confirm the e-mail address {{var:email}} with <b>{{var:appName}}</b></p><p>Click here to confirm it: {{var:link}}</p>",
-      
-    }
+emailAdapter: SimpleSendGridAdapter({
+    apiKey: process.env.SENDGRID_API_KEY,
+    fromAddress: 'ali@gmail.com',
+  })
   },
   
   liveQuery: {
