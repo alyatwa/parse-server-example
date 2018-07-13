59919405eafb44f2f659d24e0e49ed3a62835956
ali atwa <ali.atwa2040@gmail.com>
2018-07-13 10:32:06 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -4,14 +4,20 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 Parse.Cloud.afterDelete('Records', (request) => {
     // code here
-    console.log('url: ', request)
-    var file = request.get("file").url();
-    Parse.Cloud.httpRequest({
-                method: 'DELETE',
-                url: file.substring(file.lastIndexOf("/") + 1),
-                headers: {
-                    "X-Parse-Application-Id": "${process.env.APP_ID}",
-                    "X-Parse-REST-API-Key": "${process.env.MASTER_KEY}"
-                }
-                });
+    console.log('*******: ', request)
+
+    var record = new Parse.Query(Parse.Records);
+    var id = request.ParseObject.id;
+    record.equalTo("objectId", id).first().then(function (record) {
+        var file = record.get("file").url();
+        Parse.Cloud.httpRequest({
+            method: 'DELETE',
+            url: file.substring(file.lastIndexOf("/") + 1),
+            headers: {
+                "X-Parse-Application-Id": "${process.env.APP_ID}",
+                "X-Parse-REST-API-Key": "${process.env.MASTER_KEY}"
+            }
+        });
+    });
+
 })
