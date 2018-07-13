182185f478650cc06aeca6a3e291917898a5d85e
ali atwa <ali.atwa2040@gmail.com>
2018-07-13 21:57:31 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -40,7 +40,7 @@ Parse.Cloud.afterSave(Parse.User, function(request) {
   acl.setPublicReadAccess(false);
   user.setACL(acl);
     user.save({}, { useMasterKey: true }).then(function(s) {
-      console.log('[===afterSave succeeded]: '+ '@'+ user.id+'___'+s.get('objectId')+'@' /*+ JSON.stringify(s)*/);
+      console.log('[===afterSave succeeded]: ' + user.id);
     
     //console.log('#User after save#');
     var publicUser = Parse.Object.extend("PublicUser");
@@ -48,12 +48,12 @@ Parse.Cloud.afterSave(Parse.User, function(request) {
     var acel = new Parse.ACL();
     acel.setPublicReadAccess(true);
     acel.setPublicWriteAccess(false);
-    acel.setWriteAccess(s.get('objectId'),true);
-    acel.setReadAccess(s.get('objectId'),true);
+    acel.setWriteAccess(user.id,true);
+    acel.setReadAccess(user.id,true);
     PublicUser.setACL(acel);
     PublicUser.set({
       'username': s.get('username'),
-      'id': s.get('objectId')
+      'id': user.id
     })
       PublicUser.save().then(function(s) {
       console.log('user saved public')
