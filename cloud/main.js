c0317aac5986693f45102f5ff9caeae1e0010463
ali atwa <ali.atwa2040@gmail.com>
2018-07-15 01:43:27 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -135,7 +135,7 @@ Parse.Cloud.afterSave(Parse.User, function (request) {
             PublicUser.setACL(acel);
             PublicUser.set({
                 'userid': { "__type": "Pointer", "className": "_User", "objectId": user.id },
-                'userId': user.id,
+                'userId': ''+user.id+'',
                 'username': s.get('username')
             })
             PublicUser.save({}, { useMasterKey: true }).then(function (s) {
