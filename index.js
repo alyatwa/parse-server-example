1b272b7027c8a893d8616b6ce45d5a85a5a64109
ali atwa <ali.atwa2040@gmail.com>
2019-01-18 23:18:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var path = require('path');
 
 var SimpleSendGridAdapter = require('parse-server-sendgrid-adapter');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
@@ -16,7 +16,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'APP_ID',
   clientKey: process.env.myclientKey,
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed,
