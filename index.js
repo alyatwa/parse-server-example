2f8b516005f60527a81a7bfd2fdc5574cd05f381
ali atwa <ali.atwa2040@gmail.com>
2019-01-21 21:48:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,9 +108,7 @@ var httpServer = require('http').createServer(app);
 httpServer.listen(port, function(e) {
     console.log('server: '+httpServer.address().address+' # parse-server-example running on port ' + port + '.');
 });
-var http = require("http");
-setInterval(function() {
-    http.get("https://moklma.herokuapp.com");
-}, 300000);
+
+
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
