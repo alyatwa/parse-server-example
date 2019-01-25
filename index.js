bacf0d3713ec240196fb8a3ecbd0afbf7bb9998d
ali atwa <ali.atwa2040@gmail.com>
2019-01-25 19:41:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ var api = new ParseServer({
    liveQuery: {
     classNames: ['_User']
   },
+  maxUploadSize: process.env.maxUploadSize || "15mb",
   // The public URL of your app.
   // This will appear in the link that is used to verify email addresses and reset passwords.
   // Set the mount path as it is in serverURL
